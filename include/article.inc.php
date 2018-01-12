b42f145a8acaa354651a26ebe79e8d2f40905d4d
Romainrev <32880241+Romainrev@users.noreply.github.com>
2018-01-12 14:48:42 +0100

    Blog2

--- include/article.inc.php
+++ include/article.inc.php
@@ -1,1 +1,44 @@
 <?php
+
+echo("<h1>Login</h1>");
+
+if(isset($_POST['formulaire3'])) {
+    $tabErreur = array();
+
+    $titre = $_POST["titre"];
+    $chapo = $_POST["chapo"];
+    $contenu = $_POST['contenu'];
+    $date = $_POST['date'];
+
+
+    if ($_POST["titre"] == "") {
+        array_push($tabErreur, "Veuillez saisir votre titre");
+    }
+    if ($_POST["chapo"] == "") {
+        array_push($tabErreur, "Veuillez saisir votre sous titre");
+    }
+    if ($_POST["contenu"] == "") {
+        array_push($tabErreur, "Veuillez saisir votre article");
+    }
+    if ($_POST["date"] == "") {
+        array_push($tabErreur, "Veuillez saisir une date ");
+    }
+    if (count($tabErreur) != 0) {
+
+        $message = "<ul>";
+        for ($i = 0; $i < count($tabErreur); $i++) {
+            $message .= "<li>" . $tabErreur[$i] . "</li>";
+        }
+        $message .= "</ul>";
+        echo($message);
+        include("./include/formarticle.php");
+
+    } else {
+
+        $connexion = mysqli_connect("localhost", "root", "", "NFactoryBlog");
+    }
+}else{
+    include("./include/formarticle.php");
+}
+
+
