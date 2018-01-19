97bc385b9cd020c05f9488452a34ad84213fadd4
Romainrev <32880241+Romainrev@users.noreply.github.com>
2018-01-19 17:09:14 +0100

    Page d'admin

--- include/admin.inc.php
+++ include/admin.inc.php
@@ -1,24 +1,26 @@
 <?php
-
+if(isset($_SESSION['admin'])){
 if($_SESSION['admin']==1) {
-    $connexion=mysqli_connect("localhost","root","","NFactoryBlog");
+    $connexion = mysqli_connect("localhost", "root", "", "NFactoryBlog");
     $requete = "SELECT * FROM t_users";
-    $result = mysqli_query($connexion,$requete);
+    $result = mysqli_query($connexion, $requete);
     echo("<table>");
-    while($donnees=mysqli_fetch_array($result)) {
+    while ($donnees = mysqli_fetch_array($result)) {
 
-        echo("<tr><td>".$donnees['USERNAME']."</td>"."<td>".$donnees['USERFNAME']."</td>"."<td>".$donnees['USERMAIL']
-            ."</td>"."<td>".$donnees['T_ROLES_ID_ROLE']."</td>"."<td>"."<form method='post' action='#'>"."<select name='select'><option value='1'>1</option>
+        echo("<tr><td>" . $donnees['USERNAME'] . "</td>" . "<td>" . $donnees['USERFNAME'] . "</td>" . "<td>" . $donnees['USERMAIL']
+            . "</td>" . "<td>" . $donnees['T_ROLES_ID_ROLE'] . "</td>" . "<td>" . "<form method='post' action='#'>" . "<select name='select'><option value='1'>1</option>
             <option value='2'>2</option><option value='3'>3</option><option value='4'>4</option><option value='7'>7</option>
-            </select> "."</td>"."<td>"."<input type='text' name='id'><input type='submit' value='Mettre a jour' name='formulaire'>"."</form>"."</td>"."</tr>");
+            </select> " . "</td>" . "<td>" . "<input type='text' name='id'><input type='submit' value='Mettre a jour' name='formulaire'>" . "</form>" . "</td>" . "</tr>");
     }
     echo("</table>");
-    if(isset($_POST['formulaire'])){
-        $id=$_POST['id'];
-        $choix=$_POST['select'];
-        $requete2= "UPDATE t_users SET T_ROLES_ID_ROLE='$choix' WHERE ID_USER='$id'";
-        mysqli_query($connexion,$requete2);
+    if (isset($_POST['formulaire'])) {
+        $id = $_POST['id'];
+        $choix = $_POST['select'];
+        $requete2 = "UPDATE t_users SET T_ROLES_ID_ROLE='$choix' WHERE ID_USER='$id'";
+        mysqli_query($connexion, $requete2);
     }
+}
+
 } else {
     echo "erreur";
 }
