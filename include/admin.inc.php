b5676bbbbec804440c87af7478b22a7b8b7fd572
Romainrev <32880241+Romainrev@users.noreply.github.com>
2018-01-19 15:53:32 +0100

    Blog2

--- include/admin.inc.php
+++ include/admin.inc.php
@@ -7,16 +7,18 @@ if($_SESSION['admin']==1) {
     echo("<table>");
     while($donnees=mysqli_fetch_array($result)) {
 
-        echo("<td><td>".$donnees['USERNAME']."</td>"."<td>".$donnees['USERFNAME']."</td>"."<td>".$donnees['USERMAIL']."
-</td>"."<td>".$donnees["T_ROLES_ID_ROLE"]."</td>"."<td>"."<select><option value='1'>1</option><option value='2'>2</option>
-<option value='3'>3</option><option value='4'>4</option><option value='7'>7</option></select></td><td><input type='submit' 
-value='Mettre à jour' name ='Mettre à jour' </td></tr>");
-        echo('<p>');
+        echo("<tr><td>".$donnees['USERNAME']."</td>"."<td>".$donnees['USERFNAME']."</td>"."<td>".$donnees['USERMAIL']
+            ."</td>"."<td>".$donnees['T_ROLES_ID_ROLE']."</td>"."<td>"."<form method='post' action='#'>"."<select name='select'><option value='1'>1</option>
+            <option value='2'>2</option><option value='3'>3</option><option value='4'>4</option><option value='7'>7</option>
+            </select> "."</td>"."<td>"."<input type='text' name='id'><input type='submit' value='Mettre a jour' name='formulaire'>"."</form>"."</td>"."</tr>");
     }
     echo("</table>");
-    echo('<p> Vous êtes sur la page administrateur </p>');
-
-}else {
-    echo('Erreur');
-
+    if(isset($_POST['formulaire'])){
+        $id=$_POST['id'];
+        $choix=$_POST['select'];
+        $requete2= "UPDATE t_users SET T_ROLES_ID_ROLE='$choix' WHERE ID_USER='$id'";
+        mysqli_query($connexion,$requete2);
+    }
+} else {
+    echo "erreur";
 }
