df20bb46fc158ede14b91601ffe121cefad378ef
Romainrev <32880241+Romainrev@users.noreply.github.com>
2018-01-22 16:32:08 +0100

    Page d'admin

--- include/accueil.inc.php
+++ include/accueil.inc.php
@@ -4,8 +4,9 @@ $dsn ="mysql:dbname=NFactoryBlog;host=localhost;charset=utf8";
 $username = "root";
 $password ="";
 $db = new PDO($dsn, $username, $password);
-$requete = "SELECT * FROM t_articles LEFT JOIN t_categories_has_t_articles ON t_articles.ID _ARTICLE=t_categories_has_t_articles.T_CATEGORIES_ID_CATEGORIE
- ORDER BY ARTDATE DESC LIMIT 1, 5,";
+$requete ="SELECT * FROM t_articles LEFT JOIN t_categories_has_t_articles
+ ON t_articles.ID_ARTICLE=t_categories_has_t_articles.T_CATEGORIES_ID_CATEGORIE²jujulijuijkyh-ykihjkèujkujyhik:uj";
+die($requete);
 $result = $db->query($requete);
 
 while ($donnees = $result->fetch()) {
@@ -17,7 +18,7 @@ while ($donnees = $result->fetch()) {
     echo "Publié le: " . date ("F d Y H:i");
     echo('<p>');
     echo "Modifié le: " . date ("F d Y H:i", getlastmod());
-
+    echo($donnees['CATLIBELLE']);
     echo("<hr>");
 
 
