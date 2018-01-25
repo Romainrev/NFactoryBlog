ae7437cc7ff2bb2209ca95b50b96704458dc1236
Romainrev <32880241+Romainrev@users.noreply.github.com>
2018-01-25 12:07:33 +0100

    Pagination

--- include/accueil.inc.php
+++ include/accueil.inc.php
@@ -2,13 +2,42 @@
 echo("<h2>Page d'accueil</h2>");
 
 $db = connectionPDO();
+$messagesParPage= 3;
+$retourTotal= $db->query('SELECT COUNT(*) AS total FROM t_articles');
+$donneesTotal= $retourTotal->fetch(PDO::FETCH_ASSOC);
+$total=$donneesTotal['total'];
+
+$nombreDePages=ceil($total/$messagesParPage);
+
+
+
+
+if(isset($_GET['debut'])) {
+    $pageActuelle=intval($_GET['debut']);
+
+    if($pageActuelle>$nombreDePages){
+        $pageActuelle=$nombreDePages;
+    }
+} else {
+    $pageActuelle=1;
+}
+
+$premiereEntree=($pageActuelle-1)*$messagesParPage;
 
 $requete ="SELECT * FROM t_articles LEFT JOIN t_categories_has_t_articles
- ON t_articles.ID_ARTICLE=t_categories_has_t_articles.T_ARTICLES_ID_ARTICLE LEFT JOIN t_categories ON t_categories_has_t_articles.T_CATEGORIES_ID_CATEGORIE=t_categories.ID_CATEGORIE";
+ ON t_articles.ID_ARTICLE=t_categories_has_t_articles.T_ARTICLES_ID_ARTICLE 
+ LEFT JOIN t_categories ON t_categories_has_t_articles.T_CATEGORIES_ID_CATEGORIE=t_categories.ID_CATEGORIE LIMIT $premiereEntree, $messagesParPage";
 $result = $db->query($requete);
 
+
+//$retourArticle = $db ->query("SELECT * FROM t_articles LIMIT 0,3");
+
+
+
+
 echo("<p>");
-while ($donnees = $result->fetch()) {
+while ($donnees =$result->fetch()) {
+
 
     echo("<h2>");
     echo(html_entity_decode($donnees['ARTTITRE']));
@@ -20,10 +49,22 @@ while ($donnees = $result->fetch()) {
     echo($donnees['CATLIBELLE']);
     echo("<hr>");
 
-
-
-
 }
+
+echo '<p align="center">Page : ';
+for($i=1; $i<=$nombreDePages; $i++)
+{
+
+    if($i==$pageActuelle)
+    {
+        echo ' [ '.$i.' ] ';
+    }
+    else //Sinon...
+    {
+        echo ' <a href="index.php?page=accueil&amp;debut='.$i.'">'.$i.'</a> ';
+    }
+}
+echo '</p>';
 unset($db);
 
 ?>
