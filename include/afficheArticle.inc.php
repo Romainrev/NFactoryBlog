0fb073d742969d62930faeffef4b719e563ee564
Romainrev <32880241+Romainrev@users.noreply.github.com>
2018-01-30 11:44:17 +0100

    Ajout d'un flux rss

--- include/afficheArticle.inc.php
+++ include/afficheArticle.inc.php
@@ -15,3 +15,18 @@ echo("<h2>".html_entity_decode($donnees['ARTTITRE'])."</h2>");
 echo("<h3>".html_entity_decode($donnees['ARTCHAPO'])."</h3>");
 echo("<p>".html_entity_decode($donnees['ARTCONTENU'])."</p>");
 echo($share.$share3);
+
+
+
+
+/*$sql = "SELECT * FROM t_articles
+
+LEFT JOIN t_categories_has_t_articles ON t_articles.ID_ARTICLE=t_categories_has_t_articles.T_ARTICLES_ID_ARTICLE
+LEFT JOIN t_categories ON t_categories_has_t_articles.T_CATEGORIES_ID_CATEGORIE=t_categories.ID_CATEGORIE
+WHERE ID_CATEGORIE = 3 ORDER BY ID_ARTICLE DESC LIMIT 0,3";
+$reponse = $db->query($sql) or die(mysqli_error());
+
+while ($donnees= $reponse->fetch(PDO::FETCH_ASSOC)){
+    $articleId = $donnees['ID_ARTICLE'];
+
+    echo "<h2>"."<a href=\"index.php?page=articlesfull&amp;id=$articleId\">".$donnees['ARTTITRE']."</a>"."</h2>"."<h3>" .$donnees['ARTCHAPO'] ."<hr>" ."</h3>" ;
