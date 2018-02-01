950a4be439e6f986d4c57ed223a3b3f21aee59ec
Romainrev <32880241+Romainrev@users.noreply.github.com>
2018-02-01 09:54:52 +0100

    Sécurisation de la modification de mot de passe

--- include/modifPass.inc.php
+++ include/modifPass.inc.php
@@ -1,38 +1,49 @@
-<form method="post" action="">
-    <div>
-        <label for="cle"> Clé de sécurité : </label><input type="password" name="clef">
-    </div>
-    <div>
-    <label for="email">Adresse email : </label><input type="email" name="email">
-    </div>
-    <div>
-        <label for="mdp">Nouveau mot de passe : </label><input type="password" name="mdp">
-    </div>
-    <div>
-        <input type="submit" value="Valider" name="Valider">
-    </div>
-</form>
+<?php
+
+if(!isset($_GET['cle']) || !isset($_GET[('email')])){
+    echo("Accés refusé");
+}
+?>
+
+
+
 
 <?php
 
-if(isset($_POST['Valider'])){
-    $mail=$_GET['email'];
-    $cle=$_GET['cle'];
-    $clef=$_POST['clef'];
-    $clef=sha1($clef);
-    $mdp=$_POST['mdp'];
-    $mdp=sha1($mdp);
-    $email=$_POST['email'];
-    if($clef!=$cle){
-        echo("Erreur");
-    }
-    if($mail!=$email){
-        echo"Erreur";
-    }
-    else{
-        $db=connectionPDO();
-        $requete="UPDATE t_users SET USERPASSWORD='$mdp' WHERE USERMAIL='$email' ";
-        $result=$db->query($requete);
+if(isset($_GET['cle']) && isset($_GET[('email')])) {
+echo("<form method=\"post\" action=\"\">
+    <div>
+        <label for=\"cle\"> Clé de sécurité : </label><input type=\"password\" name=\"clef\">
+    </div>
+    <div>
+    <label for=\"email\">Adresse email : </label><input type=\"email\" name=\"email\">
+    </div>
+    <div>
+        <label for=\"mdp\">Nouveau mot de passe : </label><input type=\"password\" name=\"mdp\">
+    </div>
+    <div>
+        <input type=\"submit\" value=\"Valider\" name=\"Valider\">
+    </div>
+</form>");
 
+    if (isset($_POST['Valider'])) {
+        $mail = $_GET['email'];
+        $cle = $_GET['cle'];
+        $clef = $_POST['clef'];
+        $clef = sha1($clef);
+        $mdp = $_POST['mdp'];
+        $mdp = sha1($mdp);
+        $email = $_POST['email'];
+        if ($clef != $cle) {
+            echo("Erreur");
+        }
+        if ($mail != $email) {
+            echo "Erreur";
+        } else {
+            $db = connectionPDO();
+            $requete = "UPDATE t_users SET USERPASSWORD='$mdp' WHERE USERMAIL='$email' ";
+            $result = $db->query($requete);
+
+        }
     }
 }
